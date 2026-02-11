732c7690b4c69180277ab1554052e6c1798ccc80
marsya505 <marsyazulfazahrani0@gmail.com>
2026-02-11 21:49:57 +0700

    modifikasi luas segitiga

--- Week_1/src/dart_application_1/bin/dart_application_1.dart
+++ Week_1/src/dart_application_1/bin/dart_application_1.dart
@@ -2,4 +2,7 @@ import 'package:dart_application_1/dart_application_1.dart' as dart_application_
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_application_1.calculate()}!');
+// luas segitiga
+  print('Luas Segitiga: ${dart_application_1.segitiga()}');
+
 }
--- Week_1/src/dart_application_1/lib/dart_application_1.dart
+++ Week_1/src/dart_application_1/lib/dart_application_1.dart
@@ -1,3 +1,7 @@
 int calculate() {
   return 6 * 7;
 }
+// menambahkan untuk mencari luas segitiga
+double segitiga() {
+  return 0.5 * 5 * 10;
+}
