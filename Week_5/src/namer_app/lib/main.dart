9c38069b04378421591cdbd5f0d0c53f868960c2
marsya505 <marsyazulfazahrani0@gmail.com>
2026-04-06 18:26:32 +0700

    setState

--- Week_5/src/namer_app/lib/main.dart
+++ Week_5/src/namer_app/lib/main.dart
@@ -49,30 +49,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  var selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Row(
         children: [
-          Expanded(
-            child: SafeArea(
-              child: NavigationRail(
-                extended: false,
-                destinations: [
-                  NavigationRailDestination(
-                    icon: Icon(Icons.home),
-                    label: Text('Home'),
-                  ),
-                  NavigationRailDestination(
-                    icon: Icon(Icons.favorite),
-                    label: Text('Favorites'),
-                  ),
-                ],
-                selectedIndex: 0,
-                onDestinationSelected: (value) {
-                  print('selected: $value');
-                },
-              ),
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (value) {
+
+                setState(() {
+                  selectedIndex = value;
+                });
+
+              },
             ),
           ),
           Expanded(
