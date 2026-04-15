53d0d333233cba2b0425f88cbeabe05d8fd0f4d1
marsya505 <marsyazulfazahrani0@gmail.com>
2026-04-15 14:34:55 +0700

    Implementasi image section

--- Week_6/src/layout_flutter/lib/main.dart
+++ Week_6/src/layout_flutter/lib/main.dart
@@ -70,8 +70,14 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(
+        body: ListView(
           children: [
+            Image.asset(
+              'images/waterfall.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+              ),
             titleSection,
             buttonSection,
             textSection,
