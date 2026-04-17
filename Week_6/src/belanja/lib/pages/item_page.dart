6160b830cab6132ea78c2b405631bb007d5d1470
marsya505 <marsyazulfazahrani0@gmail.com>
2026-04-17 22:20:48 +0700

    Mendefinisikan Route

--- Week_6/src/belanja/lib/pages/item_page.dart
+++ Week_6/src/belanja/lib/pages/item_page.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 class ItemPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(title: Text('Item Page')),
+      body: Center(child: Text('Ini halaman Item')),
+    );
   }
 }
