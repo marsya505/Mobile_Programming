e77c1d3c26539250e73d8648df1cb5a426dbc287
marsya505 <marsyazulfazahrani0@gmail.com>
2026-04-17 17:48:05 +0700

    Nesting rows and columns

--- Week_6/src/basic_layout_flutter/lib/main.dart
+++ Week_6/src/basic_layout_flutter/lib/main.dart
@@ -9,86 +9,129 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Center(
-              child: Text(
-                'Hello World',
-                textDirection: TextDirection.ltr,
-                style: TextStyle(fontSize: 32, color: Colors.black87),
-              ),
-            ),
-            const SizedBox(height: 30),
 
-            Row(
-              mainAxisSize: MainAxisSize.min,
+    const titleText = Text(
+      'Strawberry Pavlova',
+      style: TextStyle(
+        fontWeight: FontWeight.w800,
+        letterSpacing: 0.5,
+        fontSize: 30,
+      ),
+    );
+
+    const subTitle = Text(
+      'Pavlova is a meringue-based dessert named after the Russian ballerina '
+      'Anna Pavlova. It features a crisp crust and soft, light inside.',
+      textAlign: TextAlign.center,
+      style: TextStyle(fontFamily: 'Georgia', fontSize: 18),
+    );
+
+    final stars = Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        const Icon(Icons.star, color: Colors.black),
+        const Icon(Icons.star, color: Colors.black),
+      ],
+    );
+
+    final ratings = Container(
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          stars,
+          const Text(
+            '170 Reviews',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    const descTextStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18,
+      height: 2,
+    );
+
+    // DefaultTextStyle.merge() allows you to create a default text
+    // style that is inherited by its child and all subsequent children.
+    final iconList = DefaultTextStyle.merge(
+      style: descTextStyle,
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
               children: [
-                Icon(Icons.star, color: Colors.green[500]),
-                Icon(Icons.star, color: Colors.green[500]),
-                Icon(Icons.star, color: Colors.green[500]),
-                const Icon(Icons.star, color: Colors.black),
-                const Icon(Icons.star, color: Colors.black),
+                Icon(Icons.kitchen, color: Colors.green[500]),
+                const Text('PREP:'),
+                const Text('25 min'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.timer, color: Colors.green[500]),
+                const Text('COOK:'),
+                const Text('1 hr'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.restaurant, color: Colors.green[500]),
+                const Text('FEEDS:'),
+                const Text('4-6'),
               ],
             ),
           ],
         ),
       ),
     );
-  }
-}
 
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
+    final leftColumn = Container(
+      padding: const EdgeInsets.fromLTRB(20, 30, 20, 20),
+      child: Column(
+        children: [titleText, subTitle, ratings, iconList],
       ),
-      body: Center(
+    );
 
-        child: Column(
-          
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    final mainImage = Expanded(
+      child: Image.asset(
+        'images/pict4.jfif', 
+        fit: BoxFit.cover,
+      ),
+    );
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Strawberry Pavlova Recipe')),
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Container(
+            margin: const EdgeInsets.fromLTRB(0, 40, 0, 30),
+            height: 600,
+            child: Card(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(width: 440, child: leftColumn),
+                  mainImage,
+                ],
+              ),
             ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
