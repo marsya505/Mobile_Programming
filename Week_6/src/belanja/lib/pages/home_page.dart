f8d6f68562eb57f56a46872603d675ee2adf7a37
marsya505 <marsyazulfazahrani0@gmail.com>
2026-04-17 22:38:28 +0700

    Lengkapi kode di class HomePage

--- Week_6/src/belanja/lib/pages/home_page.dart
+++ Week_6/src/belanja/lib/pages/home_page.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:belanja/models/item.dart';
 
 class HomePage extends StatelessWidget {
+  // --- KODE DARI LANGKAH 5 ---
+  // Deklarasikan variabel items di dalam class, sebelum method build
+  final List<Item> items = [
+    Item(name: 'Sugar', price: 5000),
+    Item(name: 'Salt', price: 2000)
+  ];
+  
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
