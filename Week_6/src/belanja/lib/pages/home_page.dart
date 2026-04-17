d5262944b209f22ba32375b77df18edcb9b060f7
marsya505 <marsyazulfazahrani0@gmail.com>
2026-04-17 22:40:32 +0700

    Membuat ListView dan itemBuilder

--- Week_6/src/belanja/lib/pages/home_page.dart
+++ Week_6/src/belanja/lib/pages/home_page.dart
@@ -2,16 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:belanja/models/item.dart';
 
 class HomePage extends StatelessWidget {
-  // --- KODE DARI LANGKAH 5 ---
-  // Deklarasikan variabel items di dalam class, sebelum method build
   final List<Item> items = [
     Item(name: 'Sugar', price: 5000),
     Item(name: 'Salt', price: 2000)
   ];
-  
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Belanja'),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(8),
+        child: ListView.builder(
+          padding: EdgeInsets.all(8),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+              return Card(
+                child: Container(
+                  margin: EdgeInsets.all(8),
+                  child: Row(
+                    children: [
+                      Expanded(child: Text(item.name)),
+                      Expanded(
+                        child: Text(
+                          item.price.toString(),
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+          },
+        ),
+      ),
+    );
   }
 }
